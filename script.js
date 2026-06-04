6bc26f2f6fbf16d93b06e8a73fb4fc328de3ae5a
Lorenzzo <lorenzzoleifeld@gmail.com>
2026-06-04 11:35:10 -0300

    Arrumei o filtro de tipo e o jeito como os dinossauros aparecem.

--- script.js
+++ script.js
@@ -30,18 +30,24 @@ const input = document.querySelector(".input")
 const botao = document.querySelector("#btn")
 const resultado = document.querySelector(".resultados")
 const selectPeriodo = document.getElementById("periodo")
+const selectTipo = document.getElementById("tipo")
 botao.addEventListener("click", () => {
     const periodoSelecionado = selectPeriodo.value
+    const tipoSelecionado = selectTipo.value
     const busca = input.value.trim().toLowerCase()
     const dinossauroEncontrado = dinossauros.filter(dino => dino.nome.toLowerCase().includes(busca) && (
         periodoSelecionado === "todos" ||
         dino.periodo === periodoSelecionado
     )
-    
+    &&
+    (
+        tipoSelecionado === "todos" ||
+        dino.tipo === tipoSelecionado
+    )
 ) 
    
     if(dinossauroEncontrado.length > 0) {
-    const dinossaurosPassados = dinossauroEncontrado.map(dino => dino.nome)
+    const dinossaurosPassados = dinossauroEncontrado.map(dino => `${dino.nome} - ${dino.tipo} - ${dino.periodo} `)
     resultado.textContent = dinossaurosPassados.join(", ")
     } else {
         resultado.textContent = "Dinossauro não encontrado."
