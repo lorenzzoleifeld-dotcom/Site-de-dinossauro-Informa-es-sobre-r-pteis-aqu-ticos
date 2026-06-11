b947617912e2c17bf6aff51f53d0fdddcea363c3
Lorenzzo <lorenzzoleifeld@gmail.com>
2026-06-11 11:50:23 -0300

    Criei os cards pequenos de dinossauro dinamicamente com JS.

--- script.js
+++ script.js
@@ -2,30 +2,54 @@ const dinossauros = [
     {
     nome: "Tiranossauro Rex",
     tipo: "terrestre",
-    periodo: "cretaceo"
+    periodo: "cretaceo",
+    imagem: "imgs/rex.png"
 
     },
     {
     nome: "Mosassauro",
     tipo: "aquatico",
-    periodo: "cretaceo-superior"
+    periodo: "cretaceo-superior",
+    imagem: "imgs/mosassauro.png"
     },
     {
     nome: "Ictiossauro",
     tipo: "aquatico",
-    periodo: "triassico"
+    periodo: "triassico",
+    imagem: "imgs/ictiossauro.png"
     },
     {
     nome: "Velociraptor",
     tipo: "terrestre",
-    periodo: "cretaceo-superior"
+    periodo: "cretaceo-superior",
+    imagem: "imgs/velociraptor.png"
     },
     {
     nome: "Pterodactilo",
     tipo: "aereo",
-    periodo: "jurassico-superior"
+    periodo: "jurassico-superior",
+    imagem: "imgs/pterodactilo.png"
+    },
+    {
+    nome: "Gigantossauro",
+    tipo: "terrestre",
+    periodo: "cretaceo-superior",
+    imagem: "imgs/gigantossauro.png"
+    },
+    {
+    nome: "Anquilossauro",
+    tipo: "terrestre",
+    periodo: "cretaceo",
+    imagem: "imgs/anquilossauro.png"
+    },
+    {
+    nome: "Plesiossauro",
+    tipo: "aquatico",
+    periodo: "cretaceo",
+    imagem: "imgs/plesiossauro2.png"
     }
 ]
+const cards = document.querySelector(".cards")
 const input = document.querySelector(".input")
 const botao = document.querySelector("#btn")
 const resultado = document.querySelector(".resultados")
@@ -53,6 +77,20 @@ botao.addEventListener("click", () => {
         resultado.textContent = "Dinossauro não encontrado."
     }
     
-  
 
+})
+dinossauros.forEach((dino) => {
+    const card = document.createElement("div")
+    const nome = document.createElement("h3")
+    const imagem = document.createElement("img")
+    const divImagem = document.createElement("div")
+    divImagem.classList.add("imagem")
+    card.classList.add("card")
+    nome.classList.add("nome")
+    imagem.classList.add("imagem")
+    imagem.src = dino.imagem;
+    nome.textContent = dino.nome;
+    card.appendChild(imagem)
+    card.appendChild(nome)
+    cards.appendChild(card)
 })
