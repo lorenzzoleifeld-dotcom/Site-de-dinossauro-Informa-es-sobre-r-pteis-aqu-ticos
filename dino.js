457ac0559edbebf558a661f59dafc52d7dcd7bca
Lorenzzo <lorenzzoleifeld@gmail.com>
2026-06-18 11:56:46 -0300

    feat: descrição, descoberta epaleoecologia de alguns dinossauros adicionados.

--- dino.js
+++ dino.js
@@ -14,7 +14,10 @@ const dinossauros = [
     dieta: "presas vivas",
     genero: "Tyrannosaurus",
     imagem: "imgs/rex.png",
-    pagina: "dino.html"
+    pagina: "dino.html",
+    descriçao: "Foi um dos maiores carnívoros da hístoria, e um dos dinossauros mais famosos, possuia cerca de 60 dentes afiados, braços proporcionalmente pequenos, porém robustos, pesava cerca de 6 a 9 toneladas, e viveu no final do período cretáceo.",
+    descoberta: "Os primeiros fósseis encontrados foram no ano de 1874, em Colorado, mas os fósseis iniciais só foram desenterrados no ano de 1902, em Wyoming, que foi nomeada 3 anos depois, pelo paleontólogo Henry Fairfield Osborn.",
+    paleoecologia: "Estuda como o Trex interagia com o seu ecossistema. Vivia no antigo continente ilha de Laramidia, na América do Norte, acredita se que ele viveu entre 66 e 68 milhões de anos atrás, e que exercia forte controle populacional sobre grandes herbívoros"
     },
     {
     id: "mosassauro",
@@ -28,7 +31,10 @@ const dinossauros = [
     peso: "15T",
     altura: "2 metros",
     comprimento: "15 metros",
-    imagem: "imgs/mosassauro.png"
+    imagem: "imgs/mosassauro.png",
+    descriçao: "Foi um dos maiores répteis marinhos do período Cretáceo Superior, medindo entre 12 e 18 metros e pesando até 15 toneladas, possuia vários dentes afiados, nadadeiras robustas, mandíbulas articuladas e corpo hidrodinâmico, seu nome significa 'lagarto do rio Mosa', em homenagem a Holanda, onde foram encontrados os primeiros fósseis.",
+    descoberta: "Foi descoberto em 1764, no Monte Saint Peter, quando fragmentos de crânio foram escavados, próximo ao rio Mosa, na Holanda, e foi em 1808 que o famoso cientista Georges Cuvier analisou e determinou que se tratava de um réptil gigante extinto, que ajudou a consolidar o conceito de extinção na ciência.",
+    paleoecologia: "Revela que ele dominou os oceanos no final do período Cretáceo Superior como um predador de topo, a cerca de 80 a 68 milhões de anos atrás, eles ocupavam mares rasos e quentes do planeta, com tamanhos que variavam entre 3 e 17 metros, caçavam tartarugas, grandes peixes, cefalópodes, e até Mosassauros."
     },
     {
     id: "ictiossauro",
@@ -56,13 +62,16 @@ const dinossauros = [
     peso: "20kg",
     altura: "0.6 metros",
     comprimento: "2 metros",
-    imagem: "imgs/velociraptor.png"
+    imagem: "imgs/velociraptor.png",
+    descriçao: "Foi um dinossauro carnívoro de porte médio a pequeno que viveu no período Cretáceo Superior, ele era razoalmente pequeno e leve, pesando cerca de 20kg, tinha uma garra curva e retratíl, medindo cerca de 6,5cm, e era um predador bem rápido, podendo atingit até 64km/h.",
+    descoberta: "Foi descoberto no dia 11 de agosto do ano de 1923, pelo explorador Peter Kaisen, durante uma expedição do Museu Americano de História Natural às Falésias Flamejantes, no Deserto de Gobi, na Mongólia, foi descrito oficialmente um ano depois, em 1924.",
+    paleoecologia: "Estuda como ele interagia com seu habitat e outros animais, tinha uma visão binocular, e garras que serviam para agarrar e imobilizar presas de pequeno porte, fósseis de seu antebraço revela que era coberto por penas, indicando que eram animais de sangue quente e de alta atividade metabólica."
     },
     {
     id: "pterodactilo",
-    nome: "Pterodactilo",
+    nome: "Pterodáctilo",
     tipo: "aereo",
-    periodo: "jurássico-superior",
+    periodo: "Jurássico-Superior",
     genero: "Pterodactylus",
     familia: "Pterodactylidae",
     dieta: "carnívoro",
@@ -70,7 +79,10 @@ const dinossauros = [
     peso: "3kg",
     altura: "0,5 metros",
     comprimento: "1 metro",
-    imagem: "imgs/pterodactilo.png"
+    imagem: "imgs/pterodactilo.png",
+    descriçao: "Foi um réptil voador que viveu no final do período Jurássico Superior, há cerca de 150 milhões de anos atrás, contia um bico estreito, com cerca de 90 dentes, e, diferente dos filmes, sua envergadura tinha aproximadamente 0.50 centímetros a 1,50 metros",
+    descoberta: "Os primeors fósseis foram encontrados no final do século XVIII(18), na região da Baviera, na Alemanha, e, no Brasil, foram descobertas mais de 40 espécies de Pterodáctilos, no Paraná e no Ceará",
+    paleoecologia: "Era um réptil voador, que viveu no final do período Jurássico Superior, era especializado em caçar peixes e pequenos animais em ambientes aquáticos, provavelmente captura suas presas enquanto voava a cima da água ou quando caminhava em águas rasas na beira de lagos"
     },
     {
     id: "gigantossauro",
@@ -98,7 +110,10 @@ const dinossauros = [
     peso: "8T",
     altura: "2 metros",
     comprimento: "10 metros",
-    imagem: "imgs/anquilossauro.png"
+    imagem: "imgs/anquilossauro.png",
+    descriçao: "Foi um herbívoro que viveu no final do período Cretáceo, a cerca de 68 milhões de anos atrás na América do Norte, pode atingi até 10 metros de comprimento, e é conhecido e famoso pelo seu corpo blindado e por sua poderosa maçã óssea na ponta da cauda.",
+    descoberta: "Foi descoberto em 1906, na Formação Hell Creek, em Montana (EUA), por uma expedição liberada pelo paleontólogo Barnum Brown, seu nome científico é Ankylosaurus Magniventris.",
+    paleoecologia: "Era um herbívoro de grande porte (6 a 9 metros), habitou a América do Norte no final do período Cretáceo, vivia em florestas, planícies de inundação, em climas quentes e úmidos e próximos aos rios."
     },
     {
     id: "plesiossauro",
@@ -128,6 +143,9 @@ const ovosDino = document.getElementById("ovos-dino")
 const alturaDino = document.getElementById("altura-dino")
 const pesoDino = document.getElementById("peso-dino")
 const dietaDino = document.getElementById("dieta-dino")
+const descobertaDino = document.getElementById("descoberta-dino")
+const descricaoDino = document.getElementById("descricao-dino")
+const paleoecologiaDino = document.getElementById("paleoecologia-dino")
 familiaDino.textContent =  dinoEncontrado.familia
 nomeDino.textContent = dinoEncontrado.nome;
 imagemDino.src = dinoEncontrado.imagem;
@@ -137,4 +155,7 @@ pesoDino.textContent = dinoEncontrado.peso;
 generoDino.textContent = dinoEncontrado.genero;
 ovosDino.textContent = dinoEncontrado.ovos;
 comprimentoDino.textContent = dinoEncontrado.comprimento;
+descobertaDino.textContent = dinoEncontrado.descoberta;
+descricaoDino.textContent = dinoEncontrado.descriçao;
+paleoecologiaDino.textContent = dinoEncontrado.paleoecologia;
 eraDino.textContent = dinoEncontrado.periodo.replaceAll("-", " ")
